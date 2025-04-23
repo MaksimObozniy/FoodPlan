5d865355d2515ac8334f79f961a85b47d038e648
MaksimObozniy <mobozn@gmail.com>
2025-04-23 19:26:29 +0400

    create models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Models):
+    title = models.CharField(max_length=50, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    img_url = models.ImageField(upload_to="recipes_photo", verbose_name="Фото блюда")
+    ingredients = models.TextField(verbose_name="Ингредиенты")
+    instructions = models.TextField(verbose_name="Способ приготовления")
+    
+    def __str__(self):
+        return self.title
+    
+class BotUser(models.Model):
+    telegram_id = models.BigIntegerField(unique=True, verbose_name="Telegram ID")
+    username = models.CharField(max_length=255, null=True, blank=True, verbose_name="Username")
+    is_subscribed = models.BooleanField(default=False, verbose_name="Подписка активна")
+    free_request_left = models.IntegerField(default=3, verbose_name="Осталось бесплатных подписок")
+    
+    def __str__(self):
+        return f"{self.username or self.telegram_id}"
