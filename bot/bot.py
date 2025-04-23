a22a0c2997c50ce0d2dcf5fc379d0992e442b4b5
MaksimObozniy <mobozn@gmail.com>
2025-04-23 20:02:24 +0400

    fixed bugs with start bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,13 +1,18 @@
 import os
-import django
 import asyncio
+import sys
 from dotenv import load_dotenv
+
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FoodPlan.settings')
+import django
+django.setup()
+
+
 from aiogram import Bot, Dispatcher
 from handlers import router
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FoodPlan.settings')
-django.setup()
 
 load_dotenv()
 TOKEN = os.getenv('TG_BOT_TOKEN')
