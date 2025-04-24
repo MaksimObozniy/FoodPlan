7cc45155ba370a26b8a3c5b6f2d576a06c9c6477
MaksimObozniy <mobozn@gmail.com>
2025-04-24 18:04:21 +0400

    update utils.py

--- bot/utils.py
+++ bot/utils.py
@@ -1,9 +1,23 @@
-
-from asgiref.sync import sync_to_async
+from datetime import datetime
 from recipes.models import BotUser
+from asgiref.sync import sync_to_async
+
+DAILY_LIMIT = 3
 
 @sync_to_async
-def check_and_use_access(telegram_id, username):
-    user, _ = BotUser.objects.get_or_create(telegram_id=telegram_id)
+def check_and_use_access(user_id: int, username: str) -> bool:
+    user, created = BotUser.objects.get_or_create(telegram_id=user_id)
     user.username = username
-    return user.try_use_feature()
+
+    today = datetime.now().date()
+    if user.last_request_date != today:
+        user.requests_today = 0
+        user.last_request_date = today
+
+    if user.requests_today >= DAILY_LIMIT:
+        user.save()
+        return False
+
+    user.requests_today += 1
+    user.save()
+    return True
